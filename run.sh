c7350342bfb5f07f5de7b22a7889c963a219afcc
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-07-08 11:20:40 +0200

    Changed run.sh so that the daemon can connect to the network.

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --hostname="slurm_controller_daemon" -v /home/slurm_head_docker/etc/slurm_conf:/etc/slurm head
+docker run --hostname="slurm_controller_daemon" --network slurm-network -v /home/slurm_head_docker/etc/slurm_conf:/etc/slurm head
