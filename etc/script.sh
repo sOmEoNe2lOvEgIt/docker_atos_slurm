9eb3221a70ec2c664135193df5614d07e490e9a8
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-07-26 13:35:18 +0200

    Added node logs folder for shared logs.

--- etc/script.sh
+++ etc/script.sh
@@ -2,5 +2,7 @@
 chmod 700 etc/munge/munge.key
 chown 999 /etc/munge/munge.key
 chown slurm /var/log/slurm/
+mkdir /node_logs
+chmod 700 /node_logs
 su -s /bin/bash -c "munged --key-file /etc/munge/munge.key" munge
 slurmctld -Dvvv
